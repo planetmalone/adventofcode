91db28332a7200921a3a12425b850d5202a3cfbc
Sean Malone <sean.malone@aspiraconnect.com>
2025-12-12 09:49:39 -0600

    Day 5 part 2

--- 2025/day5.ts
+++ 2025/day5.ts
@@ -1,68 +1,50 @@
 import { fetchInput } from '../fetch';
 
 (async () => {
-  // const input = await fetchInput(
-  //   '2025',
-  //   5,
-  //   '53616c7465645f5fb1d44b5620f51effa8eeb723c29637e2b42e96f0518bb450a47b32ab866f1379afe0f8a9c38364c6ca9f620fecf5c7edc28a557249eea61a'
-  // );
-  const input = `3-5
-10-14
-16-20
-12-18
+  const input = await fetchInput(
+    '2025',
+    5,
+    '53616c7465645f5fb1d44b5620f51effa8eeb723c29637e2b42e96f0518bb450a47b32ab866f1379afe0f8a9c38364c6ca9f620fecf5c7edc28a557249eea61a'
+  );
+//   const input = `3-5
+// 10-14
+// 16-20
+// 12-18
 
-1
-5
-8
-11
-17
-32`;
+// 1
+// 5
+// 8
+// 11
+// 17
+// 32`;
 
   const [rangeString, idString] = input.split('\n\n');
   
   const ranges = rangeString.split('\n').map((range: string) => {
     const parts = range.split('-');
     return { min: Number(parts[0]), max: Number(parts[1]) };
-  });
+  })
+  .sort((a, b) => a.min - b.min);
   
-  const trimmedRanges: typeof ranges = [];
-  let updated = true;
+  const reduced = [ranges[0]];
   
-  while (updated) {
-    updated = false;
+  for (let i = 1; i < ranges.length; i++) {
+    const range = ranges[i];
+    const last = reduced[reduced.length - 1];
     
-    for (const range of ranges) {
-      let skip = false;
-    
-      for (const trimmed of trimmedRanges) {
-        if (trimmed.min <= range.min && range.max <= trimmed.max) {
-          console.log("Subset: ", range, trimmed);
-          skip = true;
-          break;
-        }
-      
-        if (trimmed.min <= range.min && range.min <= trimmed.max && range.max >= trimmed.max) {
-          console.log("Expand right: ", range, trimmed);
-          trimmed.max = range.max;
-          skip = true;
-          break;
-        }
-      
-        if (trimmed.min <= range.max && range.max <= trimmed.max && range.min <= trimmed.min) {
-          console.log("Expand left: ", range, trimmed);
-          trimmed.min = range.min;
-          skip = true;
-          break;
-        }
-      }
-    
-    
-      if (!skip) {
-        trimmedRanges.push(range);
-      }
-      console.log(trimmedRanges);
+    if (range.min <= last.max + 1) {
+      console.log(`Min: ${range.min} is less than Last: ${last.max}, so extend it.`);
+      last.max = Math.max(last.max, range.max);
+    } else {
+      console.log(`[${range.min},${range.max}] not in range. Add it.`)
+      reduced.push(range);
     }
   }
   
-  console.log(trimmedRanges);
+  let total = 0;
+  for (const range of reduced) {
+    total += range.max - range.min + 1;
+  }
+  
+  console.log(total);
 })();
